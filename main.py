402ea80fedcfc4d08ebe81448717a9d2e83c8e93
spfave <sebpfave@gmail.com>
2020-12-21 20:32:07 -0500

    Create user

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": "fa,ZQ8H+4v9;xX?J4~<(7",
+    "username": "seabass721",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# Create user
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
