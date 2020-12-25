72de1d211a68b76538424d872782aa8d84c9299b
spfave <sebpfave2@gmail.com>
2020-12-24 21:14:03 -0500

    Post pixel to graph

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
 import requests
+from requests.models import Response
 load_dotenv()
 
 
 # Constants
-token = os.getenv("TOKEN")
-username = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
+USERNAME = os.getenv("USERNAME")
+GRAPH_ID = "graph1"
 
 
 # Endpoint
@@ -15,8 +17,8 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 # Create user
 user_params = {
-    "token": token,
-    "username": username,
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
@@ -25,18 +27,27 @@ user_params = {
 
 
 # Create new Pixela graph
-graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Hiking Graph",
     "unit": "Miles",
     "type": "float",
     "color": "momiji",
 }
-
 headers={
-    "X-USER-TOKEN": token,
+    "X-USER-TOKEN": TOKEN,
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+# Post pixel to graph
+hiking_graph_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+pixel_data = {
+    "date": "20201224",
+    "quantity": "10.0",
+}
+
+response = requests.post(url=hiking_graph_endpoint, json=pixel_data, headers=headers)
 print(response.text)
